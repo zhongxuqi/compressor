a375a9052160ac6b4eb51aa7766f5f0997e6a34b
zhongxuqi <zhongxuqi@foxmail.com>
2020-12-08 00:07:45 +0800

    add umeng sdk

--- android/app/src/main/kotlin/com/musketeer/compressor/MyApplication.kt
+++ android/app/src/main/kotlin/com/musketeer/compressor/MyApplication.kt
@@ -1,9 +1,18 @@
 package com.musketeer.compressor
 
+import android.util.Log
+import com.umeng.analytics.MobclickAgent
+import com.umeng.commonsdk.UMConfigure
 import io.flutter.app.FlutterApplication
 
 class MyApplication : FlutterApplication() {
+    companion object {
+        val TAG = "MyApplication"
+    }
+
     override fun onCreate() {
         super.onCreate()
+        UMConfigure.init(this, "5fce4e9dbed37e4506c546b0", "main", UMConfigure.DEVICE_TYPE_PHONE, "")
+        MobclickAgent.setPageCollectionMode(MobclickAgent.PageMode.AUTO)
     }
 }
