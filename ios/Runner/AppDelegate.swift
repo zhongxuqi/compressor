46cf3de3d1026eb3054306a0302f6c78bfc1b853
zhongxuqi <zhongxuqi@foxmail.com>
2020-12-21 09:33:06 +0800

    add video picker

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,6 +1,8 @@
 import UIKit
 import Flutter
 import HandyJSON
+import StoreKit
+import MobileCoreServices
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -16,6 +18,8 @@ import HandyJSON
         let compressorChannel = FlutterMethodChannel(name: "com.musketeer.compressor", binaryMessenger: controller.binaryMessenger)
         compressorChannel.setMethodCallHandler({(call: FlutterMethodCall, result: @escaping FlutterResult) -> Void in
             switch call.method {
+            case "feedback":
+                SKStoreReviewController.requestReview()
             case "pick_file":
                 let req = call.arguments as! [String: String]
                 self.pickFile(mimeType: req["mime_type"]!, result: result)
@@ -32,6 +36,13 @@ import HandyJSON
         if mimeType.starts(with: "image") {
             self.result = result
             imagePicker.sourceType = .photoLibrary
+            
+            imagePicker.mediaTypes = [kUTTypeImage as String]
+            controller.present(imagePicker, animated: true)
+        } else if mimeType.starts(with: "video") {
+            self.result = result
+            imagePicker.sourceType = .photoLibrary
+            imagePicker.mediaTypes = [kUTTypeMovie as String, kUTTypeVideo as String, kUTTypeMPEG4 as String, kUTTypeAVIMovie as String]
             controller.present(imagePicker, animated: true)
         }
     }
@@ -43,8 +54,21 @@ extension AppDelegate: UIImagePickerControllerDelegate, UINavigationControllerDe
         didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any]
     ) {
         let controller: FlutterViewController = window?.rootViewController as! FlutterViewController
-        if let pickedImage = info[.originalImage] as? UIImage {
-            pickedImage.jpegData(compressionQuality: 1.0)
+        if let videoURL = info[.mediaURL] as? NSURL {
+            let fileName = "\(TimeUtils.getMillisecondsSince1970()).\(String(describing: videoURL.pathExtension))"
+            let fileURL = NSTemporaryDirectory().appending(fileName)
+            do {
+                FileManager.default.createFile(atPath: fileURL, contents: nil, attributes: nil)
+                try FileManager.default.copyItem(atPath: videoURL.absoluteString!, toPath: fileURL)
+            } catch let error {
+                print("Failed to copyItem with error: \(error.localizedDescription)")
+            }
+            let fileInfo = FileInfo()
+            fileInfo.fileName = fileName
+            fileInfo.uri = fileURL
+            result?([fileInfo].toJSONString())
+            controller.dismiss(animated: true)
+        } else if let pickedImage = info[.originalImage] as? UIImage {
             let fileName = "\(TimeUtils.getMillisecondsSince1970()).jpg"
             let fileURL = NSTemporaryDirectory().appending(fileName)
             FileManager.default.createFile(atPath: fileURL, contents: pickedImage.jpegData(compressionQuality: 1.0), attributes: nil)
